ec3c7725d93f7eb563d59b1d84f638b3090ac056
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-10-11 13:30:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,17 @@
 length = float(input('Please enter the length of the room in feet (ex: 15):      '))
 width = float(input('Please enter the width of the room in feet (ex: 20):      '))
 cost_per_yard = float(input('Please enter the cost per yard of the carpet between $2.00 and $4.50 (do not include the "$" sign):      '))
+
 STATE_TAX = 0.06
 
 # Process
 yards_needed = length * width / 3   # finds the area and converts to yards
 subtotal = yards_needed * cost_per_yard     # Multiplys area by cost
-grand_total = (subtotal * STATE_TAX) + subtotal     # Adds sales tax
+tax = subtotal * STATE_TAX      # Finds sales tax
+grand_total = tax + subtotal     # Adds sales tax
 
 # Output
-print(f'This room has an area of {yards_needed:,.3f}yd\u00b2. The subtotal is ${subtotal:,.2f}, Including salestax you will have to pay ${grand_total:,.2f}')
+print(f' The area of the room is is {yards_needed:,.3f}')
+print(f' The order subtotal is ${subtotal:,.2f}')
+print(f' The order will be taxed for {tax:,.2f}')
+print(f' The order grand total is ${grand_total:,.2f}')
