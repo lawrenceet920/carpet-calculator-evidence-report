547a14c633722293fc7c269de3f33529fe01d45f
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-11-01 13:20:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ length = float(input('Please enter the length of the room in feet (ex: 15):     
 width = float(input('Please enter the width of the room in feet (ex: 20):      '))
 cost_per_yard = float(input('Please enter the cost per yard of the carpet between $2.00 and $4.50 (do not include the "$" sign):      '))
 
-STATE_TAX = 0.06
+STATE_TAX_RATE = 0.06
 
 # Process
-yards_needed = length * width / 3   # finds the area and converts to yards
+yards_needed = (length * width) / 9   # finds the area and converts to yards
 subtotal = yards_needed * cost_per_yard     # Multiplys area by cost
-tax = subtotal * STATE_TAX      # Finds sales tax
+tax = subtotal * STATE_TAX_RATE      # Finds sales tax
 grand_total = tax + subtotal     # Adds sales tax
 
 # Output
